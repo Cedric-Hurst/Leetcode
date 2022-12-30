77bc79417a0bad9d04236d4af30abcc8f99ae2fa
Cedric Hurst <33428682+Cedric-Hurst@users.noreply.github.com>
2022-12-30 12:07:36 -0500

    Update Valid Anagram Leetcode.js

--- Valid Anagram Leetcode.js
+++ Valid Anagram Leetcode.js
@@ -3,10 +3,8 @@
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 */
 var isAnagram = function(s, t) {
-    const sArr = s.split('');
-    const tArr = t.split('');
-    sArr.sort();
-    tArr.sort();
+    const sArr = s.split('').sort();
+    const tArr = t.split('').sort();
 
     if(sArr.toString() === tArr.toString()){
         return true;

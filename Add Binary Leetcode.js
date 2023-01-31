9e9f5d553c24e425fbc047bd42aaa86f0ec21fe0
Cedric Hurst <33428682+Cedric-Hurst@users.noreply.github.com>
2023-01-30 20:42:13 -0500

    Add files via upload

---
+++ Add Binary Leetcode.js
@@ -0,0 +1,9 @@
+/*
+
+Given two binary strings a and b, return their sum as a binary string.
+
+*/
+
+const addBinary = function(a, b) {
+    return (BigInt(`0b${a}`)+BigInt(`0b${b}`)).toString(2);
+}
